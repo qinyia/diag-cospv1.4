e9a368a4acf636353a53f98ed2f4e65652a3a6ef
qinyia <qinyi_linxuan@126.com>
2019-04-07 21:40:42 +0800

    compare between v1.3 and v1.4 by adding figure plotting and outputing a log file: compare.log

--- compare.sh
+++ compare.sh
@@ -11,6 +11,7 @@ ncase=${#casename[@]}
 echo $ncase
 
 pncdump=F
+diffs=T
 
 cd $workdir
 
@@ -21,15 +22,17 @@ if [ $pncdump == "T" ] ; then
 			dirtmp=${dirs[icase]}
 			ncdump -h $dirtmp/${casename[icase]}.cam.h0.0001-01.nc | tee ${casename[icase]}.cam.h0.0001-01.nc.ncdump
 			ncdump -h $dirtmp/${casename[icase]}.cam.h1.0001-01-31-00000.nc | tee ${casename[icase]}.cam.h1.0001-01-31-00000.nc.ncdump
-			ncdump -h $dirtmp/${casename[icase]}.cam.h2.0001-01-31-00000.nc | tee ${casename[icase]}.cam.h2.0001-01-31-00000.nc.ncdump
+#			ncdump -h $dirtmp/${casename[icase]}.cam.h2.0001-01-31-00000.nc | tee ${casename[icase]}.cam.h2.0001-01-31-00000.nc.ncdump
 		done
 fi
 
 #====================================== ncdump first to check all variables ==========================================
-#excludev=(ATB532_CAL,DBZE_CS,MOL532_CAL,SCOPS_OUT,CLDHGH_CAL_ICE,CLDHGH_CAL_LIQ,CLDHGH_CAL_UN,CLDLOW_CAL_ICE,CLDLOW_CAL_LIQ,CLDLOW_CAL_UN,CLDMED_CAL_ICE,CLDMED_CAL_LIQ,CLDMED_CAL_UN,CLDTOT_CAL_ICE,CLDTOT_CAL_LIQ,CLDTOT_CAL_UN,CLD_CAL_ICE,CLD_CAL_LIQ,CLD_CAL_TMP,CLD_CAL_TMPICE,CLD_CAL_TMPLIQ,CLD_CAL_TMPUN,CLD_CAL_UN)
-excludev=(ATB532_CAL,DBZE_CS,MOL532_CAL,SCOPS_OUT)
+excludev=(ATB532_CAL,DBZE_CS,MOL532_CAL,SCOPS_OUT,CLDHGH_CAL_ICE,CLDHGH_CAL_LIQ,CLDHGH_CAL_UN,CLDLOW_CAL_ICE,CLDLOW_CAL_LIQ,CLDLOW_CAL_UN,CLDMED_CAL_ICE,CLDMED_CAL_LIQ,CLDMED_CAL_UN,CLDTOT_CAL_ICE,CLDTOT_CAL_LIQ,CLDTOT_CAL_UN,CLD_CAL_ICE,CLD_CAL_LIQ,CLD_CAL_TMP,CLD_CAL_TMPICE,CLD_CAL_TMPLIQ,CLD_CAL_TMPUN,CLD_CAL_UN)
+#excludev=(ATB532_CAL,DBZE_CS,MOL532_CAL,SCOPS_OUT)
 
-ncdiff -O -x -v ${excludev} ${dirs[1]}/${casename[1]}.cam.h0.0001-01.nc ${dirs[0]}/${casename[0]}.cam.h0.0001-01.nc diff.cam.h0.nc
-ncdiff -O ${dirs[1]}/${casename[1]}.cam.h1.0001-01-31-00000.nc ${dirs[0]}/${casename[0]}.cam.h1.0001-01-31-00000.nc diff.cam.h1.nc
-ncdiff -O ${dirs[1]}/${casename[1]}.cam.h2.0001-01-31-00000.nc ${dirs[0]}/${casename[0]}.cam.h2.0001-01-31-00000.nc diff.cam.h2.nc
+if [ $diffs == "T" ] ; then
+	ncdiff -O -x -v ${excludev} ${dirs[1]}/${casename[1]}.cam.h0.0001-01.nc ${dirs[0]}/${casename[0]}.cam.h0.0001-01.nc diff.cam.h0.nc
+	ncdiff -O ${dirs[1]}/${casename[1]}.cam.h1.0001-01-31-00000.nc ${dirs[0]}/${casename[0]}.cam.h1.0001-01-31-00000.nc diff.cam.h1.nc
+#	ncdiff -O ${dirs[1]}/${casename[1]}.cam.h2.0001-01-31-00000.nc ${dirs[0]}/${casename[0]}.cam.h2.0001-01-31-00000.nc diff.cam.h2.nc
+fi
 
